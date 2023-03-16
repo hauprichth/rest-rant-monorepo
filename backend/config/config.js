365cf30ed3e30cd5ec90a2d21e0ee4a292848d6d
hauprichth <hauprichth@gmail.com>
2023-03-15 21:27:18 -0400

    finished bcrypt

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,7 +6,9 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": "127.0.0.1",
+    "port":"8080",
     "dialect": "postgres"
+    
   },
   "test": {
     "username": "root",
